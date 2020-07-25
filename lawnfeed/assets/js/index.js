7ce9edba07f102bfbb8ac6f77514ce3b81e69076
Lumiq Creative <contact@lumiqcreative.com>
2020-07-25 15:06:57 +0200

    Refresh and Refactor ‘Get Lawnfeed’ Page

--- lawnfeed/assets/js/index.js
+++ lawnfeed/assets/js/index.js
@@ -1,37 +1,21 @@
-var btn = document.getElementById("btn-download");
-var p = document.getElementById("release-name");
-
-        window.addEventListener("load", function() {
-          loadJSON("https://storage.codebucket.de/lawnchair/version.json",
-            function(data) {
-              var fileName = "Lawnfeed-" + data.app_version + ".apk";
-              var fileUrl = "https://storage.codebucket.de/lawnchair/" + data.app_version + "/" + fileName;
-              btn.setAttribute("href", fileUrl);
-              btn.setAttribute("download", fileName);
-              p.innerHTML = "Version <i>" + data.app_version + "</i>";
-            },
-            
-            function(error) {
-              btn.setAttribute("href", "https://status.razex.de");
-              p.innerHTML = "No Connection";
-            }
-          );
-        });
-    
-        function loadJSON(path, success, error) {
-          var xhr = new XMLHttpRequest();
-          xhr.onreadystatechange = function() {
-          if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-              if (success)
-                success(JSON.parse(xhr.responseText));
-              } else {
-              if (error)
-                error(xhr);
-              }
-            }
-          };
-    
-          xhr.open("GET", path, true);
-          xhr.send();
-        }
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var parsedResponse = JSON.parse(this.responseText);
+    document.querySelector(".hero__get-button").href =
+      "https://storage.codebucket.de/lawnchair/" +
+      parsedResponse.app_version +
+      "/" +
+      "Lawnfeed-" +
+      parsedResponse.app_version +
+      ".apk";
+    document.querySelector(".hero__version-label").innerHTML =
+      parsedResponse.app_version + ".";
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://storage.codebucket.de/lawnchair/version.json",
+  true
+);
+xmlhttp.send();
