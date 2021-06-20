6f19379d59754a364e0b794f57ccf2b3dc572c48
Raphael Tang <raphpb1912@gmail.com>
2021-06-21 02:57:05 +0800

    clean

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  future: {
-    webpack5: true,
-  },
-  webpack: function (config, options) {
-    config.experiments = {}
-    return config
-  },
-}
--- utils/nav.ts
+++
@@ -1,5 +0,0 @@
-export const navLinks = [
-  { name: "About", href: "/about" },
-  { name: "Contact", href: "/contact" },
-  { name: "Download", href: "/download" },
-]
