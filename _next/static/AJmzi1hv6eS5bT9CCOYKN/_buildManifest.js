e8518f499541cd5e63624a8b495b934f6d8abefd
raphtlw <47694127+raphtlw@users.noreply.github.com>
2021-06-21 14:02:52 +0000

    Deploying to preview-gh-pages from @ LawnchairLauncher/lawnchair.app@a5f6f549adb51783fdeab19e41510c022b212232 🚀

---
+++ _next/static/AJmzi1hv6eS5bT9CCOYKN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/845-d1300ac0a1d2a1fff3b6.js","static/chunks/pages/index-dc3995ceb3bcb475f374.js"],"/_error":["static/chunks/pages/_error-94ed2348718d59e1af9b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
