d0570b1a1973344b299d1b48531c01e8bb698ef4
superdragonxd <70206496+SuperDragonXD@users.noreply.github.com>
2023-06-05 20:38:51 +0800

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Lawnchair</span>,
+  project: {
+    link: 'https://github.com/LawnchairLauncher/lawnchair'
+  },
+  docsRepositoryBase: 'https://github.com/LawnchairLauncher/lawnchair.app',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
