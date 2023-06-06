5be3dc01c4604e6acb37b14f2ea0cf2e00f992ed
superdragonxd <70206496+SuperDragonXD@users.noreply.github.com>
2023-06-06 20:14:49 +0800

    progress

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import Image from 'next/image'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Link from 'next/link'
 
 const config: DocsThemeConfig = {
-  logo: <span>Lawnchair</span>,
+  logo: <span className='gap-2 flex items-center'>
+      <Image
+      className="rounded-full"
+      src="/images/lawnchair.png"
+      alt=""
+      height={30}
+      width={30}
+      />
+      <span className='font-bold'>Lawnchair</span>
+  </span>,
   project: {
     link: 'https://github.com/LawnchairLauncher/lawnchair'
   },
   docsRepositoryBase: 'https://github.com/LawnchairLauncher/lawnchair.app',
   footer: {
-    text: '(c) Lawnchair',
+    text: (<>
+      <span>Website by <a href="https://superdragonxd.github.io/">SuperDragonXD</a> - Hosted by <a href="https://razex.de">Razex.de</a> - Public Analytics on <a href="https://simpleanalytics.com/lawnchair.app">Simple Analytics</a> - View <Link href="/privacy_policy">Privacy Policy</Link></span>
+    </>),
   },
   head: (
     <>
@@ -16,7 +29,10 @@ const config: DocsThemeConfig = {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
-  )
+  ),
+  chat: {
+    link: 'https://discord.gg/3x8qNWxgGZ'
+  }
 }
 
 export default config
