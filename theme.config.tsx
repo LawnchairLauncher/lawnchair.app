8c661bdfaf85f530d85144e029bf0cd0f9a653ee
superdragonxd <70206496+SuperDragonXD@users.noreply.github.com>
2023-06-07 20:53:44 +0800

    tweaks

--- theme.config.tsx
+++ theme.config.tsx
@@ -27,6 +27,7 @@ const config: DocsThemeConfig = {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <link rel="icon" href="/favicon.ico" />
     </>
   ),
